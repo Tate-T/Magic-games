0ae5c22df29cb2de10bbb6ada724e2caccfaeb20
DimaVax <126974388+DimaVax@users.noreply.github.com>
2024-09-23 19:47:18 +0200

    Підключив паршали до файлів

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-// import "./js/menu";
+import "./js/starter-modal.js";
+import "./js/header.js";
+import "./js/hero.js";
+import "./js/leap-year.js";
+import "./js/computer-number.js";
+import "./js/rps.js";
+import "./js/calculator.js";
+import "./js/time-calculator.js";
+import "./js/dino.js";
+import "./js/football.js";
+import "./js/biggest-number.js";
+import "./js/slider.js";
+import "./js/footer.js";
+import "./js/footer-modal.js";
