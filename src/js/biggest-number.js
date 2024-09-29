bdcdf93a20fb248d6c3515971e44f394a60ab9af
kleryxx <126974469+Vlad-Lipinskyi@users.noreply.github.com>
2024-09-29 17:45:42 +0300

    biggest-number-fix

--- src/js/biggest-number.js
+++ src/js/biggest-number.js
@@ -1,13 +1,16 @@
-document.getElementById("checkButton").addEventListener("click", function() {
+function checkBiggestNumber() {
     const num1 = parseFloat(document.getElementById("number1").value);
     const num2 = parseFloat(document.getElementById("number2").value);
     const num3 = parseFloat(document.getElementById("number3").value);
-    
+
     if (isNaN(num1) || isNaN(num2) || isNaN(num3)) {
         document.getElementById("result").textContent = "Будь ласка, введіть дійсні числа.";
-        return;
+    } else {
+        const biggestNumber = Math.max(num1, num2, num3);
+        document.getElementById("result").textContent = "Найбільше число: " + biggestNumber;
     }
+}
 
-    const biggestNumber = Math.max(num1, num2, num3);
-    document.getElementById("result").textContent = "Найбільше число: " + biggestNumber;
-});
+document.getElementById("number1").addEventListener("blur", checkBiggestNumber);
+document.getElementById("number2").addEventListener("blur", checkBiggestNumber);
+document.getElementById("number3").addEventListener("blur", checkBiggestNumber);
