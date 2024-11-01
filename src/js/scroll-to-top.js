0b26121c7659b72346a8b275f6419507300ad5f9
Maksym Ryaboshapka <e.ryaboshapka777@gmail.com>
2024-11-01 17:21:26 +0200

    add dark-mode

--- src/js/scroll-to-top.js
+++ src/js/scroll-to-top.js
@@ -1,11 +1,18 @@
 const scrollToTopEl = document.querySelector(".scroll");
 
 window.onscroll = () => {
-    const pageOffset = window.pageYOffset;
+  const pageOffset = window.pageYOffset;
 
-    if (pageOffset >= 1000) {
-        scrollToTopEl.style.bottom = '50px';
-    } else {
-        scrollToTopEl.style.bottom = '-100vh';
-    }
+  if (pageOffset >= 1000) {
+    scrollToTopEl.style.bottom = "50px";
+  } else {
+    scrollToTopEl.style.bottom = "-100vh";
+  }
 };
+
+const themeChangerEl = document.querySelector(".header__theme-changer");
+const arrowEl = document.querySelector(".scroll__icon");
+
+themeChangerEl.addEventListener("change", () => {
+  arrowEl.src = "./images/dark-mode-arrow.webp";
+});
